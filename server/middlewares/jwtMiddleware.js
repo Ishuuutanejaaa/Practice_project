0f0d48ac914448d7903e2d84d349d3cfcbf67998
Ishuuutanejaaa <ishutaneja3110@gmail.com>
2024-11-08 21:51:36 +0530

    tenth_part_two

--- server/middlewares/jwtMiddleware.js
+++ server/middlewares/jwtMiddleware.js
@@ -47,72 +47,72 @@
 // //   }
 // // });
 
-// var jwt=require('jsonwebtoken');
-// const generateToken=(userData)=>{
-//     // in this function we are creating new fresh jwt token to provide user,for login/session management or for authorisation purpose.
-//     return jwt.sign(userData,process.env.PRIVATE_KEY)
-// }
-// const validateJwtToken=(req,res,next)=>{
-//     // first we are checking that jwt token is available or not 
-//     const authorization= req.headers.authorization;
-//     // output1 = bearer jadgjirgjawiufgjvci - bearer + string 
-//     // output2 = wertgerthbrfgfeg - string 
-//     // output3=   " "   - empty string 
-//     // output4 = token bana hi nhi hai local ho ya end point testing se bheja ho - without token header send kara h
-//     if (!authorization){
-//         return res.status(401).json({err:'token not available'});
-//     }
-
-//     // we are storing the token value from headers and spliting to get "bearer xyz.abc.kjh" to "xyz.abc.kjh"
-
-//     const token = req.headers.authorization.split(' ')[1]
-
-// //  token aaya but unauthorised hai (if token provided is wrong)
-//     if(!token){
-//         return res.status(401).json({err:'Unauthorized User'});
-//     }
-
-//     try{
-//         // in this error handler try catch we are handling , if token is validated or verified , 
-//         // then move to next middleware or respond back to client .
-//         const validateToken=jwt.verify(token,process.env.PRIVATE_KEY);
-//         req.user=validateToken;
-//         next();
-//     }catch(err){
-//         console.error("error occured: ",err.message);
-//     }
-// }
-
-// module.exports={generateToken,validateJwtToken}
-
-
-
-
-const jwt = require('jsonwebtoken');
-
-const generateToken = (userData) => {
-    return jwt.sign(userData, process.env.PRIVATE_KEY);
-};
-
-const jwtAuthMiddleware = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
-        return res.status(401).json({ err: 'Token not available' });
+var jwt=require('jsonwebtoken');
+const generateToken=(userData)=>{
+    // in this function we are creating new fresh jwt token to provide user,for login/session management or for authorisation purpose.
+    return jwt.sign(userData,process.env.PRIVATE_KEY)
+}
+const validateJwtToken=(req,res,next)=>{
+    // first we are checking that jwt token is available or not 
+    const authorization= req.headers.authorization;
+    // output1 = bearer jadgjirgjawiufgjvci - bearer + string 
+    // output2 = wertgerthbrfgfeg - string 
+    // output3=   " "   - empty string 
+    // output4 = token bana hi nhi hai local ho ya end point testing se bheja ho - without token header send kara h
+    if (!authorization){
+        return res.status(401).json({err:'token not available'});
     }
 
-    const token = authorization.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({ err: 'Unauthorized User' });
+    // we are storing the token value from headers and spliting to get "bearer xyz.abc.kjh" to "xyz.abc.kjh"
+
+    const token = req.headers.authorization.split(' ')[1]
+
+//  token aaya but unauthorised hai (if token provided is wrong)
+    if(!token){
+        return res.status(401).json({err:'Unauthorized User'});
     }
 
-    try {
-        const validateToken = jwt.verify(token, process.env.PRIVATE_KEY);
-        req.user = validateToken;
+    try{
+        // in this error handler try catch we are handling , if token is validated or verified , 
+        // then move to next middleware or respond back to client .
+        const validateToken=jwt.verify(token,process.env.PRIVATE_KEY);
+        req.user=validateToken;
         next();
-    } catch (err) {
-        console.error("Error occurred: ", err.message);
-        return res.status(403).json({ err: 'Invalid token' });
+    }catch(err){
+        console.error("error occured: ",err.message);
     }
-};
+}
 
-module.exports = { jwtAuthMiddleware, generateToken };
+module.exports={generateToken,validateJwtToken}
+
+
+
+
+// const jwt = require('jsonwebtoken');
+
+// const generateToken = (userData) => {
+//     return jwt.sign(userData, process.env.PRIVATE_KEY);
+// };
+
+// const jwtAuthMiddleware = (req, res, next) => {
+//     const authorization = req.headers.authorization;
+//     if (!authorization) {
+//         return res.status(401).json({ err: 'Token not available' });
+//     }
+
+//     const token = authorization.split(' ')[1];
+//     if (!token) {
+//         return res.status(401).json({ err: 'Unauthorized User' });
+//     }
+
+//     try {
+//         const validateToken = jwt.verify(token, process.env.PRIVATE_KEY);
+//         req.user = validateToken;
+//         next();
+//     } catch (err) {
+//         console.error("Error occurred: ", err.message);
+//         return res.status(403).json({ err: 'Invalid token' });
+//     }
+// };
+
+// module.exports = { jwtAuthMiddleware, generateToken };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,29 +1,29 @@
-// const express=require("express");
-// const router=express.Router();
-// import {jwtAuthMiddleware} from "../middlewares/jwtMiddleware";
-// // const { jwtAuthMiddleware } = require("../middlewares/jwtMiddleware"); // Use require instead of import
-// const{
-//     registerUser,
-//     loginUser
-// }=require("../controllers/userController");
+const express=require("express");
+const router=express.Router();
+import {jwtAuthMiddleware} from "../middlewares/jwtMiddleware";
+// const { jwtAuthMiddleware } = require("../middlewares/jwtMiddleware"); // Use require instead of import
+const{
+    registerUser,
+    loginUser
+}=require("../controllers/userController");
 
-// //route for registration
-// router.post("/register",registerUser);
-// //route for user login 
-// //router.post("/login",loginUser);
-// router.post("/login",jwtAuthMiddleware, loginUser);
-// module.exports=router;
+//route for registration
+router.post("/register",registerUser);
+//route for user login 
+//router.post("/login",loginUser);
+router.post("/login",jwtAuthMiddleware, loginUser);
+module.exports=router;
 
-const express = require("express");
-const router = express.Router();
-const { jwtAuthMiddleware } = require("../middlewares/jwtMiddleware"); // Use require instead of import
-const { registerUser, loginUser } = require("../controllers/userController");
+// const express = require("express");
+// const router = express.Router();
+// const { jwtAuthMiddleware } = require("../middlewares/jwtMiddleware"); // Use require instead of import
+// const { registerUser, loginUser } = require("../controllers/userController");
 
-// Route for registration
-router.post("/register", registerUser);
+// // Route for registration
+// router.post("/register", registerUser);
 
-// Route for user login
-router.post("/login", jwtAuthMiddleware, loginUser);
-// router.post("/login", loginUser); // Remove jwtAuthMiddleware from login
+// // Route for user login
+// router.post("/login", jwtAuthMiddleware, loginUser);
+// // router.post("/login", loginUser); // Remove jwtAuthMiddleware from login
 
-module.exports = router;
+// module.exports = router;
