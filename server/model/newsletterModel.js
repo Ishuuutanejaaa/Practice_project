a6d43dabc879cb26d4ba60a0dbd1d609e5248bfe
Ishuuutanejaaa <ishutaneja3110@gmail.com>
2024-11-13 11:45:39 +0530

    newsletter

---
+++ server/model/newsletterModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const newsletterSchema = mongoose.Schema(
+    {
+        title:{
+            type:String,
+            require:[true,"please add your title"],
+        },
+        author:{
+            type:String,
+            require:[true,"please add your author"],
+        },
+        date:{
+            type:String,
+            require:[true,"please add your date"],
+        },
+        imageUrl:{
+            type:Number,
+            require:[true,"please add your imageUrl"],
+        },
+        description:{
+            type:String,
+            require:[true,"please add your description"],
+        },
+    },
+    {
+        timestamps:true,
+    }
+);
+
+module.exports = mongoose.model("Newsletter",newsletterSchema);
---
+++ server/routes/newsletterRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router;
+const {getNewsletter , createNewsletter} = require("../controllers/newsletterController");
+const { validateJwtToken } = require("../middlewares/jwtMiddleware");
+
+
+router.get("/",getNewsletter);
+
+router.post("/",validateJwtToken,createNewsletter);
